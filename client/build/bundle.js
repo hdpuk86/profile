4df8d329ffaa382e800999b9c0dd2ce31eaae1fb
hdpuk86 <hayley.d.prior@gmail.com>
2018-01-02 15:55:53 +0000

    adds info on hover

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -81,16 +81,31 @@ window.addEventListener('load', onLoad);
 /* 1 */
 /***/ (function(module, exports) {
 
+var addHover = function(project, img){
+  var div = document.getElementById(`${project.name-img-div}`);
+  img.addEventListener('mouseover', function(){
+    var name = document.createElement('h2');
+    name.innerText = project.name;
+    name.className = 'projectName';
+    div.appendChild(name);
+  });
+  img.addEventListener('mouseleave', function(){
+    div.removeChild(div.lastChild);
+  });
+};
+
 var PortfolioView = function(projects) {
   var div = document.getElementById('main-container');
   projects.forEach((project) => {
     var imageDiv = document.createElement('div');
     imageDiv.className = 'project-img-div';
+    imageDiv.id = `${project.name-img-div}`;
     var img = document.createElement('img');
     img.className = 'project-img';
     img.src = project.squareImage;
     imageDiv.appendChild(img);
     div.appendChild(imageDiv);
+    addHover(project, img);
   });
 };
 
