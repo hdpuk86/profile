cab8ed5a4b787329a9082a6a1a08e00c2e5641da
hdpuk86 <hayley.d.prior@gmail.com>
2018-01-02 17:21:47 +0000

    fixes glitch and refactors

--- client/src/views/portfolioView.js
+++ client/src/views/portfolioView.js
@@ -1,28 +1,49 @@
-var addHover = function(project, img){
-  var div = document.getElementById(`${project.name}-img-div`);
-  img.addEventListener('mouseover', function(){
-    var name = document.createElement('h2');
-    name.innerText = project.name;
-    name.className = 'projectName';
-    div.appendChild(name);
+var addHover = function(project, imageDiv){
+  var name = document.getElementById(`${project.name}-name`);
+  imageDiv.addEventListener('mouseover', function(){
+    name.style.visibility = 'visible';
   });
-  img.addEventListener('mouseleave', function(){
-    div.removeChild(div.lastChild);
+  imageDiv.addEventListener('mouseleave', function(){
+    name.style.visibility = 'hidden';
   });
+};
+
+var addImageContainer = function(project){
+  var imageDiv = document.createElement('div');
+  imageDiv.className = 'project-img-div';
+  imageDiv.id = `${project.name}-img-div`;
+  return imageDiv;
+};
+
+var addImage = function(project){
+  var img = document.createElement('img');
+  img.className = 'project-img';
+  img.src = project.squareImage;
+  return img;
+};
+
+var addName = function(project){
+  var name = document.createElement('h2');
+  name.innerText = project.name;
+  name.className = 'project-name';
+  name.id = `${project.name}-name`;
+  return name;
+};
+
+var appendAll = function(div, imageDiv, img, name){
+  imageDiv.appendChild(img);
+  imageDiv.appendChild(name);
+  div.appendChild(imageDiv);
 };
 
 var PortfolioView = function(projects) {
   var div = document.getElementById('main-container');
   projects.forEach((project) => {
-    var imageDiv = document.createElement('div');
-    imageDiv.className = 'project-img-div';
-    imageDiv.id = `${project.name}-img-div`;
-    var img = document.createElement('img');
-    img.className = 'project-img';
-    img.src = project.squareImage;
-    imageDiv.appendChild(img);
-    div.appendChild(imageDiv);
-    addHover(project, img);
+    var imageDiv = addImageContainer(project);
+    var img = addImage(project);
+    var name = addName(project);
+    appendAll(div, imageDiv, img, name);
+    addHover(project, imageDiv);
   });
 };
 
