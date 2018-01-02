b2ce43693c87a23996eca58af29bee83f926ef8d
hdpuk86 <hayley.d.prior@gmail.com>
2018-01-02 15:23:55 +0000

    adds space project & fixes height

--- client/src/db/seeds.js
+++ client/src/db/seeds.js
@@ -3,6 +3,14 @@ db.dropDatabase();
 
 db.projects.insertMany([
   {
+    name: 'Big 5 Safari Slots Game',
+    type: 'Java, Android',
+    github: 'https://github.com/hdpuk86/android_slot_machine',
+    url: null,
+    mainImage: 'images/portfolio/safariSlots.png',
+    squareImage: 'images/portfolio/safariSquare.png'
+  },
+  {
     name: 'Animal Shelter',
     type: 'Ruby, Sinatra, postgreSQL',
     github: 'https://github.com/hdpuk86/Animal_shelter',
@@ -11,11 +19,11 @@ db.projects.insertMany([
     squareImage: 'images/portfolio/shelterSquare.png'
   },
   {
-    name: 'Big 5 Safari Slots Game',
-    type: 'Java, Android',
-    github: 'https://github.com/hdpuk86/android_slot_machine',
-    url: null,
-    mainImage: 'images/portfolio/safariSlots.png',
-    squareImage: 'images/portfolio/safariSquare.png'
+    name: 'Space Oddity',
+    type: 'Javascript, HTML, NoSQL',
+    github: 'https://github.com/hdpuk86/space_oddity',
+    url: 'space-oddity.herokuapp.com',
+    mainImage: 'images/portfolio/spaceOddity.png',
+    squareImage: 'images/portfolio/spaceSquare.png'
   }
 ]);
